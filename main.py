444b9fc09d682954d8f3dce0f6a5b649846f35a0
JADELKA / JDT <166043734+NioyJadelkaFp@users.noreply.github.com>
2024-09-16 14:05:38 -0300

    Cambio

--- main.py
+++ main.py
@@ -1,39 +1,19 @@
 from flask import Flask,render_template
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 import json
-import requests
-import pprint
-
-def calcular_diferencia(fecha_inicio, fecha_final):
-    diferencia:int = relativedelta(fecha_final, fecha_inicio)
-    
-    return diferencia.years, diferencia.months, diferencia.days
-
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     title:str = "Home"
-    with open('static/json/infoweb.json', 'r') as file:
-        data = json.load(file)
 
-    return render_template('index.html', data=data, title=title)
+    with open('static/json/proyectos.json', 'r') as archivo:
+        datos = json.load(archivo)
 
-@app.route('/hola')
-def hola():
-    title:str = 'Hola Mundo'
-    return render_template('hola.html',title=title)
+    return render_template('index.html', title=title, datos=datos)
 
-@app.route('/memes')
-def meme():
-    url = "https://www.reddit.com/r/memexico/.json"
-    headers = {'User-agent': 'your bot 0.1'}
-    response = requests.get(url, headers=headers)
-    data = response.json()
-    memes = data['data']['children'][:1000]  # Obtener los primeros 10 memes
-    return render_template('memes.html', memes=memes)
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
