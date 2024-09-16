112f6034c599adf13d2d16db1da1ef7738479b3b
JADELKA / JDT <166043734+NioyJadelkaFp@users.noreply.github.com>
2024-09-16 14:23:44 -0300

    prueba

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from flask import Flask,render_template
-import json
+#import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    title = "Home"
-    with open('static/json/proyectos.json', 'r') as archivo:
-            datos = json.load(archivo)
-    return render_template('index.html', title=title, datos=datos)
+    #title = "Home"
+    #with open('static/json/proyectos.json', 'r') as archivo:
+           # datos = json.load(archivo)
+    #return render_template('index.html', title=title, datos=datos)
+    return "index"
 
 if __name__ == '__main__':
     app.run(debug=True)
